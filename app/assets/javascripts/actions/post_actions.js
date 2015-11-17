78329559cf8cbffed0221cec4c8dacf8065c6abb
Carl Baron <carl.baron@gmail.com>
2015-11-17 11:59:28 -0500

    Add PostActions.

---
+++ app/assets/javascripts/actions/post_actions.js
@@ -0,0 +1,15 @@
+var PostActions = {
+  receivePosts: function(posts) {
+    AppDispatcher.dispatch({
+      actionType: PostConstants.RECEIVE_POSTS,
+      posts: posts
+    });
+  },
+
+  receivePost: function(post) {
+    AppDispatcher.dispatch({
+      actionType: PostConstants.RECEIVE_POST,
+      post: post
+    });
+  }
+};
