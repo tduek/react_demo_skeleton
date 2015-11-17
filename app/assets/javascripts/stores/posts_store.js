e6cf21cc00c9522bb38fb011e0e10228a3ccc16b
Carl Baron <carl.baron@gmail.com>
2015-11-17 12:16:50 -0500

    Update PostsStore in response to dispatches.

--- app/assets/javascripts/stores/posts_store.js
+++ app/assets/javascripts/stores/posts_store.js
@@ -2,17 +2,50 @@
   var _posts = [];
   var CHANGE_EVENT = "change";
 
+  var _updatePost = function(newPost) {
+    var oldPost = PostsStore.findPostById(newPost.id);
+    if (oldPost) {
+      _posts.splice(_posts.indexOf(oldPost), 1);
+    }
+
+    _posts.push(newPost);
+  };
+
   root.PostsStore = $.extend({}, EventEmitter.prototype, {
 
-    addEventHandler: function(callback) {
+    addChangeHandler: function(callback) {
       this.on(CHANGE_EVENT, callback);
     },
 
-    removeEventHandler: function() {
+    removeChangeHandler: function() {
       this.removeListener(CHANGE_EVENT, callback);
     },
 
+    all: function() {
+      return _posts.slice();
+    },
+
     dispatcherId: AppDispatcher.register(function(payload) {
-    })
+      switch (payload.actionType) {
+      case PostConstants.RECEIVE_POSTS:
+        _posts = payload.posts
+        PostsStore.emit(CHANGE_EVENT);
+        break;
+      case PostConstants.RECEIVE_POST:
+        _updatePost(payload.post);
+        PostsStore.emit(CHANGE_EVENT);
+        break;
+      }
+    }),
+
+   findPostById: function(id) {
+     for (var i = 0; i < _posts.length; i++) {
+       if (_posts[i].id === id) {
+         return _posts[i];
+       }
+     }
+
+       return;
+     }
   });
 })(this);
