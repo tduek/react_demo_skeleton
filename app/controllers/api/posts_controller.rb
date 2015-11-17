fd06370c289f1d911d68d75b40bf151daa104276
Carl Baron <carl.baron@gmail.com>
2015-11-17 10:06:24 -0500

    Add PostsController Actions.

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,11 +1,23 @@
 class Api::PostsController < ApplicationController
 
   def index
+    @posts = Post.all
+    render json: @posts
   end
 
   def create
+    @post = Post.create!(post_params)
+    render json: @post
   end
 
   def show
+    @post = Post.find(params[:id])
+    render json: @post
   end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:title)
+    end
 end
